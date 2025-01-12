64ccb9216c6c1edbd8a1f415320f317a6420735a
juyun <juyun571@naver.com>
2025-01-12 16:55:14 +0900

    250112

---
+++ Lv0/접미사인지 확인하기.js
@@ -0,0 +1,52 @@
+// 어떤 문자열에 대해서 접미사는 특정 인덱스부터 시작하는 문자열을 의미합니다.
+// 예를 들어, "banana"의 모든 접미사는 "banana", "anana", "nana", "ana", "na", "a"입니다.
+// 문자열 my_string과 is_suffix가 주어질 때,
+// is_suffix가 my_string의 접미사라면 1을, 아니면 0을 return 하는 solution 함수를 작성해 주세요.
+
+// 입출력 예 #1
+
+// 예제 1번에서 is_suffix가 my_string의 접미사이기 때문에 1을 return 합니다.
+// 입출력 예 #2
+
+// 예제 2번에서 is_suffix가 my_string의 접미사가 아니기 때문에 0을 return 합니다.
+// 입출력 예 #3
+
+// 예제 3번에서 is_suffix가 my_string의 접미사가 아니기 때문에 0을 return 합니다.
+// 입출력 예 #4
+
+// 예제 4번에서 is_suffix가 my_string의 접미사가 아니기 때문에 0을 return 합니다.
+
+// 입출력 예
+// my_string	is_suffix	result
+// "banana"	"ana"	1
+// "banana"	"nan"	0
+// "banana"	"wxyz"	0
+// "banana"	"abanana"	0
+
+solution("banana", "ana");
+solution("banana", "nan");
+solution("banana", "wxyz");
+solution("banana", "abanana");
+
+//방법1
+function solution(my_string, is_suffix) {
+  var answer = [];
+
+  for (let i = 0; i < my_string.length; i++) {
+    answer.push(my_string.slice(i));
+  }
+
+  answer.includes(is_suffix);
+
+  return answer.includes(is_suffix) ? 1 : 0;
+}
+
+//endsWith 메서드를 사용하면 문자열이 특정 문자열로 끝나는지 확인할 수 있다.
+//방법2
+function solution(my_string, is_suffix) {
+  // endsWith 메서드를 사용하여 접미사 여부를 판단
+  if (my_string.endsWith(is_suffix)) {
+    return 1; // 접미사라면 1 반환
+  }
+  return 0; // 아니면 0 반환
+}
